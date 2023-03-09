34352087999e6a397f0dd5878cdf920d7648ec04
Sebatian Shader <sebfumaster@aol.com>
2023-03-09 12:45:24 +0000

    Complete Challenge Pig Latin

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,5 +1,11 @@
+import java.util.*;
 
 public class PigLatin {
+    static Set<Character> vowels;
+    public PigLatin() {
+        Character[] vArray = {'a', 'e', 'i', 'o', 'u'};
+        vowels = new HashSet<Character>(Arrays.asList(vArray));
+    }
     /**
      * Pig latin works as follows: every word has its first character moved to the end, and 'ay' is added to the end of the
      * word.
@@ -17,6 +23,13 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        char first = in.charAt(0);
+        char second;
+        if(in.length() == 1 || vowels.contains(first))
+            return in + "way";
+        second = in.charAt(1);
+        if(vowels.contains(second))
+            return in.substring(1) + first + "ay";
+        return in.substring(2) + first + second + "ay";
     }
 }
